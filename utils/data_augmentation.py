d71f2e1f720b7904d3f4147889584d822df1be11
unknown <straybird23@126.com>
2024-02-02 08:38:10 -0500

    first commit

---
+++ utils/data_augmentation.py
@@ -0,0 +1,108 @@
+import os
+
+import cv2
+import shutil
+import numpy as np
+import albumentations as A
+
+from utils.utils import list_file_r
+from PIL import Image
+
+
+def batch_augment_with_bbox(images_path:os.PathLike, bbox_path:os.PathLike, augmentor:A.Compose=None, target_folder:os.PathLike='augmented', random_choice:float=0.2) -> None:
+    """
+
+    """
+    # get img path files
+    img_extension = '.tif'
+    img_pfs = [f for f in os.listdir(images_path) if os.path.splitext(f)[-1] == img_extension]
+    if random_choice:
+        rng = np.random.default_rng(seed=12345)
+        sample_size = int(len(img_pfs)*random_choice)
+        img_pfs = rng.choice(img_pfs, size=sample_size, replace=False)
+    
+    for img_pf in img_pfs:
+        filename = os.path.splitext(os.path.split(img_pf)[-1])[0]
+        img_filename = filename + '.tif'
+        img_dst_pf = os.path.normpath(os.path.join(images_path, target_folder, img_filename))
+        # get corresponding bbox path files
+        bbox_filename = filename + '.txt'
+        bbox_pf = os.path.normpath(os.path.join(bbox_path, bbox_filename))
+        bbox_dst_pf = os.path.normpath(os.path.join(bbox_path, target_folder, bbox_filename))
+        # put class label to the end
+        bboxes = np.loadtxt(bbox_pf, delimiter=' ')
+        bboxes = np.concatenate((bboxes, bboxes[:,0:1]), axis=-1)[:,1:]
+        # get img
+        img_pf = os.path.normpath(os.path.join(images_path, img_filename))
+        img = cv2.imread(img_pf)
+        # augment
+        augmented = augmentor(image=img,bboxes=bboxes)
+        # swap back class label
+        bboxes = np.array(augmented['bboxes'])
+        bboxes = np.concatenate((bboxes[:,-1:], bboxes), axis=-1)[:,:-1]
+        # save augmented image and bounding boxes
+        img = Image.fromarray(augmented['image'])
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
+        if not os.path.exists(os.path.split(img_dst_pf)[0]):
+            os.makedirs(os.path.split(img_dst_pf)[0])
+        if not os.path.exists(os.path.split(bbox_dst_pf)[0]):
+            os.makedirs(os.path.split(bbox_dst_pf)[0])
+        img.save(img_dst_pf)
+        num_of_points=4
+        np.savetxt(bbox_dst_pf, bboxes, fmt=' '.join(['%i']+['%1.4f']*num_of_points))
+
+    return
+
+
+def batch_augment_with_mask(images_path:os.PathLike, mask_path:os.PathLike, augmentor:A.Compose=None, target_folder:os.PathLike='augmented', random_choice:float=0.2) -> None:
+    """
+
+    """
+    # get img path files
+    img_extension = '.tif'
+    img_pfs = [f for f in os.listdir(images_path) if os.path.splitext(f)[-1] == img_extension]
+    if random_choice:
+        rng = np.random.default_rng(seed=12345)
+        sample_size = int(len(img_pfs)*random_choice)
+        img_pfs = rng.choice(img_pfs, size=sample_size, replace=False)
+    
+    for img_pf in img_pfs:
+        filename = os.path.splitext(os.path.split(img_pf)[-1])[0]
+        img_filename = filename + '.tif'
+        img_dst_pf = os.path.normpath(os.path.join(images_path, target_folder, img_filename))
+        # get corresponding mask path files
+        
+        #mask_filename = filename + '.tif'
+        mask_pf = os.path.normpath(os.path.join(mask_path, img_filename))
+        mask_dst_pf = os.path.normpath(os.path.join(mask_path, target_folder, img_filename))
+        # get img
+        img_pf = os.path.normpath(os.path.join(images_path, img_filename))
+        img = cv2.imread(img_pf)
+        # mask img
+        mask_pf = os.path.normpath(os.path.join(mask_path, img_filename))
+        mask = cv2.imread(mask_pf)
+        # augment
+        augmented = augmentor(image=img,mask=mask)
+        # save augmented image and masks
+        img = Image.fromarray(augmented['image']).convert('L')
+        #img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
+        mask = Image.fromarray(augmented['mask']).convert('L')
+        #mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY) 
+        if not os.path.exists(os.path.split(img_dst_pf)[0]):
+            os.makedirs(os.path.split(img_dst_pf)[0])
+        if not os.path.exists(os.path.split(mask_dst_pf)[0]):
+            os.makedirs(os.path.split(mask_dst_pf)[0])
+        img.save(img_dst_pf)
+        mask.save(mask_dst_pf)
+
+    return
+
+
+
+
+
+
+
+
+
+
---
+++ utils/utils.py
@@ -0,0 +1,143 @@
+import os
+import cv2
+import shutil
+from pathlib import Path
+
+def list_file_r(path:os.PathLike, extension:list=None) -> list:
+    """
+    Args
+
+        path: PathLike object
+
+    Return
+
+        list of files under path, excluding directories
+    """
+    listed_files = []
+    for root, dirs, files in os.walk(path):
+        for name in files:
+            # if file formats are provided, only return files with the given formats
+            if extension and os.path.splitext(name)[-1] in extension:
+                pathfile = os.path.join(root, name)
+                listed_files.append(os.path.normpath(pathfile))
+            elif not extension:
+                pathfile = os.path.join(root, name)
+                listed_files.append(os.path.normpath(pathfile))
+    return listed_files
+
+def mask_to_contour(mask_path:os.PathLike):
+    msk = cv2.imread(mask_path)
+    msk = cv2.cvtColor(msk, cv2.COLOR_BGR2GRAY)
+    _, im = cv2.threshold(msk, 100, 255, type=cv2.THRESH_BINARY)
+    contours, hierachy = cv2.findContours(im, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    return contours
+
+def contour_to_YOLO_annotation(contours, width, height, destination):
+    annotation_text = ''
+    for contour in contours[:-1]:
+        annotation_text += '0'
+        for d in range(len(contour)):
+            annotation_text += ' '
+            X, Y = contour[d][0]
+            X, Y = X/width, Y/height
+            annotation_text += '{} {}'.format(X, Y)
+        annotation_text += '\n' # newline
+    #with open()
+    return
+
+def copy_image_to_sub_dir(image_pathfiles:list, sub_dir:str, copy_bbox=False):
+    image_data_path = os.path.split(image_pathfiles[0])[0]
+    last_images_folder_index = image_data_path.rfind('images')
+    annotation_path = image_data_path[:last_images_folder_index] + 'labels' +  image_data_path[last_images_folder_index+6:]
+    if copy_bbox:
+        bbox_path = image_data_path[:last_images_folder_index] + 'bbox' +  image_data_path[last_images_folder_index+6:]
+    for img_pf in image_pathfiles:
+        #print(img_pf)
+        annotation_pf = os.path.splitext(os.path.split(img_pf)[-1])[0] + '.txt'
+        #print(annotation_pf)
+        #annotation_pf = 'MASK' + annotation_pf[3:]
+        annotation_pf = os.path.normpath(os.path.join(annotation_path, annotation_pf))
+        annotation_filename = os.path.split(annotation_pf)[-1]
+        annotation_dst_pf = os.path.normpath(os.path.join(annotation_path, sub_dir, annotation_filename))
+
+        img_filename = os.path.split(img_pf)[-1]
+        img_dst_pf = os.path.normpath(os.path.join(image_data_path, sub_dir, img_filename))
+        #print(img_filename)
+        #print(img_dst_pf)
+        img_dir, ann_dir=os.path.join(image_data_path, sub_dir), os.path.join(annotation_path, sub_dir)
+        
+        if not os.path.exists(img_dir):
+            os.makedirs(img_dir)
+        if not os.path.exists(ann_dir):
+            os.makedirs(ann_dir)
+
+        if copy_bbox:
+            bbox_pf = os.path.normpath(os.path.join(bbox_path, os.path.splitext(os.path.split(img_pf)[-1])[0] + '.txt'))
+            bbox_filename = os.path.split(bbox_pf)[-1]
+            bbox_dst_pf = os.path.normpath(os.path.join(bbox_path, sub_dir, bbox_filename))
+            bbox_dir = os.path.join(bbox_path, sub_dir)
+            if not os.path.exists(bbox_dir):
+                os.makedirs(bbox_dir)
+            shutil.copy2(bbox_pf, bbox_dst_pf)
+
+        shutil.copy2(img_pf, img_dst_pf)
+        shutil.copy2(annotation_pf, annotation_dst_pf)
+    return
+
+def create_yolo_annotation_from_mask(mask:os.PathLike, out:os.PathLike, add_border_on_edge = False):
+    if os.path.isfile(mask):
+        pass
+    elif os.path.isdir(mask):
+        mask_data_path = mask
+        annotation_dst = out
+        pathparts=list(Path(os.path.normpath(annotation_dst)).parts[:-1])
+        bbox_dst = os.sep.join(pathparts+['bbox'])
+        mask_pfs = list_file_r(mask_data_path, extension=['.tif'])
+        # loop over provided masks
+        for pf in mask_pfs:
+            msk = cv2.imread(pf)
+            height, width, _ = msk.shape
+            msk = cv2.cvtColor(msk, cv2.COLOR_BGR2GRAY)
+            _, im = cv2.threshold(msk, 100, 255, type=cv2.THRESH_BINARY)
+            if add_border_on_edge:
+                black_border = [0 for _ in range(1000)]
+                im[0], im[-1] = black_border, black_border
+            contours, hierachy = cv2.findContours(im, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+            # preprocess contours and bounding boxes
+            annotation_text = ''
+            #bbox_text = ''
+            for contour in contours:
+                # b-box
+                #bbox_text += '0 '
+                x,y,w,h = cv2.boundingRect(contour)
+                x,y = x+w/2, y+h/2
+                x,y,w,h = x/width, y/height, w/width, h/height
+                #bbox_text += '{} {} {} {}\n' .format(x,y,w,h)
+                # mask
+                annotation_text += '0'
+                for d in range(len(contour)):
+                    annotation_text += ' '
+                    #contour[d][0][1] -= border_height
+                    X, Y = contour[d][0]
+                    #Y = Y - border_height
+                    X, Y = X/width, Y/height
+                    annotation_text += '{} {}'.format(X, Y)
+                annotation_text += '\n' # newline
+
+            file_name = os.path.splitext(os.path.split(pf)[-1])[0]
+            annotation_fp = os.path.normpath(os.path.join(annotation_dst, file_name)+'.txt')
+            bbox_fp = os.path.normpath(os.path.join(bbox_dst, file_name)+'.txt')
+            if not os.path.exists(annotation_dst):
+                os.makedirs(annotation_dst)
+            with open(annotation_fp, 'w') as mask_annotation_file:
+                mask_annotation_file.write(annotation_text)
+                mask_annotation_file.close()
+
+            if not os.path.exists(bbox_dst):
+                os.makedirs(bbox_dst)
+            """ with open(bbox_fp, 'w') as mask_bbox_file:
+                mask_bbox_file.write(bbox_text)
+                mask_bbox_file.close() """
+
+#def yolo_to_
